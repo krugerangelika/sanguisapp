53abfcab74078868b31e3eb1b3c26715e951d06a
krugerrangelika <81249028+krugerrangelika@users.noreply.github.com>
2021-12-13 22:10:46 +0100

    new
    new

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output
 import pandas as pd
 
 # źródło: https://nordcan.iarc.fr/en/dataviz/tables?mode=year&years=1960_2019&group_populations=1
-df = pd.read_csv('nordcan_males_year.csv')
+df = pd.read_csv('nordcan_test.csv', sep=',')
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 SIDEBAR_STYLE = {
@@ -64,24 +64,24 @@ def render_page_content(pathname):
                 html.H1('Zachorowalność w krajach skandynawskich na nowotwory - Noworodki oraz Dzieci do 5 lat',
                         style={'textAlign':'center'}),
                 dcc.Graph(id='bargraph',
-                         figure=px.bar(df, barmode='group', x='Year',
-                         y=['Children 0', 'Children 5']))
+                         figure=px.bar(df, barmode='group', x='Years',
+                         y=['0', '5']))
                 ]
     elif pathname == "/page-1":
         return [
                 html.H1('Zachorowalność w krajach skandynawskich na nowotwory - Dzieci w wieku od 10 do 15 lat',
                         style={'textAlign':'center'}),
                 dcc.Graph(id='bargraph',
-                         figure=px.bar(df, barmode='group', x='Year',
-                         y=['Children 10', 'Children 15']))
+                         figure=px.bar(df, barmode='group', x='Years',
+                         y=['10', '15']))
                 ]
     elif pathname == "/page-2":
         return [
                 html.H1('Zachorowalność w krajach skandynawskich na nowotwory - Dzieci w wieku 10-15 lat',
                         style={'textAlign':'center'}),
                 dcc.Graph(id='bargraph',
-                         figure=px.bar(df, barmode='group', x='Year',
-                         y=['Adult 20', 'Adult 25']))
+                         figure=px.bar(df, barmode='group', x='Years',
+                         y=['20', '25']))
                 ]
 
 if __name__=='__main__':
