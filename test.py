453cefa73ec77473affacf44541823503f708d80
krugerrangelika <81249028+krugerrangelika@users.noreply.github.com>
2021-12-13 15:10:46 +0100

    new
    new

---
+++ test.py
@@ -0,0 +1,88 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.express as px
+from dash.dependencies import Input, Output
+import pandas as pd
+
+# źródło: https://nordcan.iarc.fr/en/dataviz/tables?mode=year&years=1960_2019&group_populations=1
+df = pd.read_csv('nordcan_males_year.csv')
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.P(
+            "Nordcan", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Page 1", href="/page-1", active="exact"),
+                dbc.NavLink("Page 2", href="/page-2", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+
+app.layout = html.Div([
+    dcc.Location(id="url"),
+    sidebar,
+    content
+])
+
+
+@app.callback(
+    Output("page-content", "children"),
+    [Input("url", "pathname")]
+)
+def render_page_content(pathname):
+    if pathname == "/":
+        return [
+                html.H1('Zachorowalność w krajach skandynawskich na nowotwory - Noworodki oraz Dzieci do 5 lat',
+                        style={'textAlign':'center'}),
+                dcc.Graph(id='bargraph',
+                         figure=px.bar(df, barmode='group', x='Year',
+                         y=['Children 0', 'Children 5']))
+                ]
+    elif pathname == "/page-1":
+        return [
+                html.H1('Zachorowalność w krajach skandynawskich na nowotwory - Dzieci w wieku od 10 do 15 lat',
+                        style={'textAlign':'center'}),
+                dcc.Graph(id='bargraph',
+                         figure=px.bar(df, barmode='group', x='Year',
+                         y=['Children 10', 'Children 15']))
+                ]
+    elif pathname == "/page-2":
+        return [
+                html.H1('Zachorowalność w krajach skandynawskich na nowotwory - Dzieci w wieku 10-15 lat',
+                        style={'textAlign':'center'}),
+                dcc.Graph(id='bargraph',
+                         figure=px.bar(df, barmode='group', x='Year',
+                         y=['Adult 20', 'Adult 25']))
+                ]
+
+if __name__=='__main__':
+    app.run_server(debug=True, port=3000)
